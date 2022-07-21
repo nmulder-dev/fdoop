ad9774a8c87e765e6b7501bba121e670f627720c
Nick Mulder <nmulder@outlook.com>
2022-07-20 20:08:22 -0500

    borrowingworking with struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![allow(unused_imports)]
 
 use std::collections::HashMap;
-use std::{env, io};
+use std::{env, io, string};
+use std::io::Error;
 use std::fs::{self, File, DirEntry};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use sha256::digest_file;
 
 fn main() -> io::Result<()> {
@@ -11,12 +12,12 @@ fn main() -> io::Result<()> {
 
     // Get args crom command line
     let args: Vec<String> = env::args().collect();
+    let path = Path::new(&args[1]);
+    let mut file_hashes: Vec<FileHash> = Vec::new();
+    let mut file_hash: &FileHash = &FileHash::new();
 
     // Check if args supplied
     if args.len() > 1 {
-        let path = Path::new(&args[1]);
-        let mut file_hashes: Vec<FileHash> = Vec::new();
-
         // Check if first arg is a directory or file
         if path.is_file() {
             println!("Please supply a directory path as first argument");    
@@ -24,14 +25,20 @@ fn main() -> io::Result<()> {
 
         // Run comparison
         println!("Comparing files in {:?}", path.display());
+        let mut entry: Result<DirEntry, Error>;
+        let mut this_path: PathBuf;
+        let mut temp: &FileHash = &FileHash::new();
         for entry in fs::read_dir(path)? {
             let entry = entry?;
             let this_path = entry.path();
             if this_path.is_file() {
-                let hash = digest_file(this_path).unwrap();
-                file_hashes.push(FileHash {path: Box::new(this_path.as_path()), hash: hash});
-                println!("{:?}", hash);
+                file_hash = &FileHash::from_path(this_path.as_path()).unwrap();
+                file_hashes.push(FileHash::from_path(this_path.as_path()).unwrap());
             }
+        }
+
+        for hash in file_hashes {
+           println!("Path: {:?} \t Hash: {:?}", hash.path, hash.hash);
         }
 
     } else {
@@ -41,8 +48,48 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+// fn read_files(path: &Path) -> &Path {
+//     let mut entry: Result<DirEntry, Error>;
+//         let mut this_path: PathBuf;
+//         let mut temp: &FileHash = &FileHash::new();
+
+//         for entry in fs::read_dir(path)? {
+//             let entry = entry?;
+//             let this_path = entry.path();
+//             if this_path.is_file() {
+//                 file_hash = &FileHash::from_path(this_path.as_path()).unwrap();
+//                 file_hashes.push(FileHash::from_path(this_path.as_path()).unwrap());
+//             } else {
+//                 read_files(path);
+//             }
+//         }
+// }
+
 #[derive(Debug)]
 struct FileHash {
-    path: Box<&'a Path>,
+    path: String,
     hash: String,
+}
+
+impl<'b> FileHash {
+    fn new()-> FileHash {
+        let new = FileHash { path: String::from("."), hash: String::from("") };
+        new
+        
+    }
+
+    fn from_path(path: &Path) -> Result<FileHash, &str> {
+        let path = path;
+        if path.is_file() {
+            let hash = digest_file(path).unwrap();
+            let fh: FileHash = FileHash { 
+            path: String::from(
+                path.to_str().unwrap_or_default()), 
+                hash: hash 
+            };
+            Ok(fh)
+        } else {
+            Err("Supplied path is not a file")
+        }
+    }
 }
